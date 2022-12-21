9e8d0071a3f1c5ddd072cb952cbb911774bfc166
josegerar <45176268+josegerar@users.noreply.github.com>
2022-12-21 12:43:42 -0500

    agregar funcion datepart nullable

--- src/EfCoreExtensions.cs
+++ src/EfCoreExtensions.cs
@@ -91,9 +91,12 @@ public static class EfCoreExtensions
     public static int? DatePart(this DateTime? date, string datePartArg) =>
         throw new InvalidOperationException($"{nameof(DatePart)} cannot be called client side.");
 
+    public static int DatePart(this DateTime date, string datePartArg) =>
+        throw new InvalidOperationException($"{nameof(DatePart)} cannot be called client side.");
+
     public static ModelBuilder AddSqlFunctions(this ModelBuilder modelBuilder)
     {
-        modelBuilder.HasDbFunction(() => DatePart(default, default!))
+        modelBuilder.HasDbFunction(() => DatePart(null, default!))
            .HasTranslation(args =>
                     new SqlFunctionExpression("DATEPART",
                         new[]                            {
@@ -103,6 +106,20 @@ public static class EfCoreExtensions
                         true,
                         new[] { false, false },
                         typeof(int?),
+                        null
+                    )
+                );
+
+        modelBuilder.HasDbFunction(() => DatePart(default, default!))
+           .HasTranslation(args =>
+                    new SqlFunctionExpression("DATEPART",
+                        new[]                            {
+                                new SqlFragmentExpression((args.ToArray()[1] as SqlConstantExpression)!.Value!.ToString()!),
+                                args.ToArray()[0]
+                        },
+                        true,
+                        new[] { false, false },
+                        typeof(int),
                         null
                     )
                 );
